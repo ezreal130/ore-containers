c940fcb32e6a5b10638f348bc2fbaa048de73e91
ezreal <24875836+ezreal130@users.noreply.github.com>
2024-04-09 16:52:39 +0800

    Update mine.sh

--- ore-miner/mine.sh
+++ ore-miner/mine.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-rpc=${SOL_RPC:-"https://api.mainnet-beta.solana.com"}
+rpc=${SOL_RPC:-""}
 priority_fee=${PRIORITY_FEE:-"5000000"}
 thread_count=${THREAD_COUNT:-"16"}
 command="/root/.cargo/bin/ore --rpc $rpc --keypair /root/wallet.json --priority-fee $priority_fee mine --threads $thread_count"
